c83d0f44141f1956f06516dca221339834a66fdc
cw7430 <cw282@naver.com>
2025-08-01 16:55:10 +0900

    오류수정

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_NAME=cw7430/next-java-server
+IMAGE_NAME=cw7430/next-java-server-sangyoonlee
 TAG=latest
 
 # 빌드
--- src/main/java/com/next/app/api/user/repository/BusinessRepository.java
+++ src/main/java/com/next/app/api/user/repository/BusinessRepository.java
@@ -4,6 +4,4 @@ import com.next.app.api.user.entity.Business;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BusinessRepository extends JpaRepository<Business, Long> {
-
-    void updateBusiness(Business business);
 }
